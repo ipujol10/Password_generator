c68258bf1d1ef12428400961fe9956a204edbef3
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-06-27 17:18:01 +0200

    UPDATE: window #2
    It creates the window and it is possible to exit

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,5 +1,11 @@
+import javax.swing.JFrame;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame("Password generator"); // Make window
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the window
+
+        frame.setVisible(true);
     }
 }
