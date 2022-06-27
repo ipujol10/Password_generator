d50c003d4a795a2e0a6aed42febef37f5181cc75
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-06-27 18:08:17 +0200

    UPDATE: length

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,15 +1,29 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
+        // Frame
         JFrame frame = new JFrame("Password generator"); // Make window
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the window
-
-        frame.setSize(600, 400);
-
-        frame.setVisible(true); // show it
-
         // frame.pack(); // allow to contain everything
+        frame.setSize(400, 400);
+
+        // Menu bar
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("Help");
+        mb.add(m1);
+
+        // Panel with the things
+        JPanel panel = new JPanel();
+        JLabel length_msg = new JLabel("Length of the password");
+        JTextField length = new JTextField(2);
+        panel.add(length_msg);
+        panel.add(length);
+
+        // Components
+        frame.getContentPane().add(BorderLayout.NORTH ,mb);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.setVisible(true); // show it
     }
 }
